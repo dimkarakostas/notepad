1541f8fa5f506843e136aa9b1ab7b08a266e5a69
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-03 14:12:04 +0300

    Add readNotes function to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import config from './config';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: []
+        };
+
+        this.readNotes = this.readNotes.bind(this);
+        this.readNotes();
+    }
+
+    readNotes() {
+        axios.get(config.BACKEND_URL + '/read')
+        .then(res => {
+            console.log(res.data);
+            this.setState({notes: res.data.notes});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <div className="App">
