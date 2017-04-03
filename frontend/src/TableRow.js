a9180ec1046d86d6bd2a13d893155da71b2a311b
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-03 14:22:11 +0300

    Add backend support for deleting note

--- frontend/src/TableRow.js
+++ frontend/src/TableRow.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import config from './config';
 
 export default class TableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteNote = this.deleteNote.bind(this);
+    }
+
+    deleteNote() {
+        axios.post(config.BACKEND_URL + '/delete', {
+            id: this.props.row.index
+        })
+        .then((res) => {
+            console.log(res);
+            this.props.rowNotes();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <tr>
